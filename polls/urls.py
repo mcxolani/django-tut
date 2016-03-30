21fd616aa60f819c885abc9b70d7910280cb4417
xolani <mcxolani@gmail.com>
2016-03-30 15:47:33 +0200

    change to rest

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	url(r'^$', views.QuestionList.as_view(), name='index'),
+	url(r'^(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view(), name='detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,24 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render
-from django.template import loader
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+from polls.models import Question
+from polls.serializers import QuestionSerializer
 
 from .models import Question
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-	try:
-		question = Question.objects.get(id=question_id)
-	except Question.DoesNotExist:
-		Http404("Question does not exist")
+class QuestionList(generics.ListCreateAPIView):
+	"""
+    List all code questions, or create a new question.
+    """
+	queryset = Question.objects.all()
+	serializer_class = QuestionSerializer
+		
 
-	return render(request, 'polls/detail.html', 'question':question)
-
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+	"""docstring for Q"""
+	queryset = Question.objects.all()
+	serializer_class = QuestionSerializer

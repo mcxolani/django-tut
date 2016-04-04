29b695d1616639510ca8645ba506ed129193ecfe
xolani <mcxolani@gmail.com>
2016-04-04 23:25:57 +0200

    add index.html

--- front-end/app/app.component.ts
+++ front-end/app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from 'angular2/core';
+import {AuthHttp, AuthConfig} from 'angular2-jwt';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+
+	constructor(public authHttp: AuthHttp) {}
+
+}
--- front-end/app/main.ts
+++ front-end/app/main.ts
@@ -1,4 +1,15 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {Http, HTTP_PROVIDERS}    from 'angular2/http';
+import {AuthHttp, AuthConfig} from 'angular2-jwt';
 
-bootstrap(AppComponent);
+
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  provide(AuthHttp, {
+    useFactory: (http) => {
+      return new AuthHttp(new AuthConfig(), http);
+    },
+    deps: [Http]
+  })
+]);

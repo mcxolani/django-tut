89cd51b4eb31e41b99c873618a3920818dce7ce1
xolani <mcxolani@gmail.com>
2016-04-04 23:18:41 +0200

    add rest jwt

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
 ]
 
